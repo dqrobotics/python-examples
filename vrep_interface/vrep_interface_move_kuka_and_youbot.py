84ca63c5821060b7c6d9567088f953569d1b4450
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2019-10-01 01:19:26 -0700

    Added the exception handling

--- vrep_interface/vrep_interface_move_kuka_and_youbot.py
+++ vrep_interface/vrep_interface_move_kuka_and_youbot.py
@@ -109,112 +109,112 @@ vi = DQ_VrepInterface()
 
 # Always use a try-catch in case the connection with V-REP is lost
 # otherwise your clientid will be locked for future use
-#try:
-# Connects to the localhost in port 19997 with timeout 100ms and 10 retries for each method call
-if not vi.connect(19997, 100, 5):
-    raise Exception("Unable to connect to vrep!")
+try:
+    # Connects to the localhost in port 19997 with timeout 100ms and 10 retries for each method call
+    if not vi.connect(19997, 100, 5):
+        raise Exception("Unable to connect to vrep!")
 
-simulation_parameters = SimulationParameters(
-    move_manipulator=True,
-    wd=0.5,
-    wn=0.1,
-    total_time=40.0,
-    dispz=0.1)
+    simulation_parameters = SimulationParameters(
+        move_manipulator=True,
+        wd=0.5,
+        wn=0.1,
+        total_time=40.0,
+        dispz=0.1)
 
-# Starts simulation in V-REP
-print("Starting V-REP simulation...")
-vi.start_simulation()
+    # Starts simulation in V-REP
+    print("Starting V-REP simulation...")
+    vi.start_simulation()
 
-# Initialize VREP robots
-lbr4p_vreprobot = LBR4pVrepRobot("LBR4p", vi)
-youbot_vreprobot = YouBotVrepRobot("youBot", vi)
+    # Initialize VREP robots
+    lbr4p_vreprobot = LBR4pVrepRobot("LBR4p", vi)
+    youbot_vreprobot = YouBotVrepRobot("youBot", vi)
 
-# Load DQ_robotics Kinematics
-lbr4p = lbr4p_vreprobot.kinematics()
-youbot = youbot_vreprobot.kinematics()
+    # Load DQ_robotics Kinematics
+    lbr4p = lbr4p_vreprobot.kinematics()
+    youbot = youbot_vreprobot.kinematics()
 
-# Initialize controllers
-lbr4p_controller = DQ_TaskSpacePseudoInverseController(lbr4p)
-lbr4p_controller.set_control_objective(ControlObjective.Pose)
-lbr4p_controller.set_gain(5)
-lbr4p_controller.set_damping(0.0)
+    # Initialize controllers
+    lbr4p_controller = DQ_TaskSpacePseudoInverseController(lbr4p)
+    lbr4p_controller.set_control_objective(ControlObjective.Pose)
+    lbr4p_controller.set_gain(5)
+    lbr4p_controller.set_damping(0.0)
 
-qp_solver = DQ_QuadprogSolver()
-youbot_controller = DQ_ClassicQPController(youbot, qp_solver)
-youbot_controller.set_control_objective(ControlObjective.Pose)
-youbot_controller.set_gain(5)
-youbot_controller.set_damping(0.01)
+    qp_solver = DQ_QuadprogSolver()
+    youbot_controller = DQ_ClassicQPController(youbot, qp_solver)
+    youbot_controller.set_control_objective(ControlObjective.Pose)
+    youbot_controller.set_gain(5)
+    youbot_controller.set_damping(0.01)
 
-sampling_time = 0.1
-tc = 0.0
-tcircle = 1 + E_ * 0.5 * 0.1 * j_
-rcircle = DQ(np.array([1.0]))
+    sampling_time = 0.1
+    tc = 0.0
+    tcircle = 1 + E_ * 0.5 * 0.1 * j_
+    rcircle = DQ(np.array([1.0]))
 
-# Get initial robot information
-lbr4p_q = np.array([0.0, 1.7453e-01, 0.0, 1.5708, 0.0, 2.6273e-01, 0.0])
-lbr4p_vreprobot.send_q_to_vrep(lbr4p_q)
-lbr4p_x0 = conj(lbr4p.reference_frame()) * lbr4p.fkm(lbr4p_q)
-youbot_q = youbot_vreprobot.get_q_from_vrep()
-
-print("Starting control loop...")
-first_iteration = True
-for t in np.arange(0.0, simulation_parameters.total_time, sampling_time):
-    # Get Obstacles from VREP
-    plane = get_plane_from_vrep(vi, "ObstaclePlane", k_)
-    cylinder1 = get_line_from_vrep(vi, "ObstacleCylinder1", k_)
-    cylinder2 = get_line_from_vrep(vi, "ObstacleCylinder2", k_)
-
-    # Set LBR4p reference
-    (lbr4p_xd_wrt_base, lbr4p_ff_wrt_base) = compute_reference(simulation_parameters, lbr4p_x0, t)
-    lbr4p_xd = lbr4p.reference_frame() * lbr4p_xd_wrt_base
-    lbr4p_ff = lbr4p.reference_frame() * lbr4p_ff_wrt_base
-
-    # YouBot Trajectory
-    youbot_xd = lbr4p_xd * (1 + 0.5 * E_ * 0.015 * k_) * j_
-    youbot_ff = lbr4p_ff * (1 + 0.5 * E_ * 0.015 * k_) * j_
-
-    # Modify the trajectory in order to draw a circle
-    if first_iteration:
-        first_iteration = False
-    elif np.linalg.norm(youbot_controller.get_last_error_signal()) < 0.025:
-        tc = tc + 0.01
-        rcircle = cos(tc / 2) + k_ * sin(tc / 2)
-
-    youbot_xd = youbot_xd * rcircle * tcircle
-    youbot_ff = youbot_ff * rcircle * tcircle
-
-    # Compute control signal for the arm
-    lbr4p_u = lbr4p_controller.compute_tracking_control_signal(lbr4p_q, vec8(lbr4p_xd), vec8(lbr4p_ff))
-
-    # Computer control signal for the youbot
-    youbot_base = youbot.get_chain(0)
-    youbot_base_pose = youbot_base.raw_fkm(youbot_q)
-    youbot__base_Jx = youbot_base.raw_pose_jacobian(youbot_q, 2)
-    (Jconstraint, bconstraint) = compute_constraints(youbot_base_pose, youbot__base_Jx, plane, cylinder1, cylinder2)
-    youbot_controller.set_inequality_constraint(-Jconstraint, 1*bconstraint)
-    youbot_u = youbot_controller.compute_tracking_control_signal(youbot_q, vec8(youbot_xd), vec8(youbot_ff))
-
-    # Desired joint values
-    lbr4p_q = lbr4p_q + lbr4p_u*sampling_time
-    youbot_q = youbot_q + youbot_u*sampling_time
-
-    # Send desired values
+    # Get initial robot information
+    lbr4p_q = np.array([0.0, 1.7453e-01, 0.0, 1.5708, 0.0, 2.6273e-01, 0.0])
     lbr4p_vreprobot.send_q_to_vrep(lbr4p_q)
-    youbot_vreprobot.send_q_to_vrep(youbot_q)
+    lbr4p_x0 = conj(lbr4p.reference_frame()) * lbr4p.fkm(lbr4p_q)
+    youbot_q = youbot_vreprobot.get_q_from_vrep()
 
-    time.sleep(0.01)
-print("Control finished...")
+    print("Starting control loop...")
+    first_iteration = True
+    for t in np.arange(0.0, simulation_parameters.total_time, sampling_time):
+        # Get Obstacles from VREP
+        plane = get_plane_from_vrep(vi, "ObstaclePlane", k_)
+        cylinder1 = get_line_from_vrep(vi, "ObstacleCylinder1", k_)
+        cylinder2 = get_line_from_vrep(vi, "ObstacleCylinder2", k_)
 
-# Stops simulation in V-REP
-print("Stopping V-REP simulation...")
-vi.stop_simulation()
+        # Set LBR4p reference
+        (lbr4p_xd_wrt_base, lbr4p_ff_wrt_base) = compute_reference(simulation_parameters, lbr4p_x0, t)
+        lbr4p_xd = lbr4p.reference_frame() * lbr4p_xd_wrt_base
+        lbr4p_ff = lbr4p.reference_frame() * lbr4p_ff_wrt_base
 
-# Disconnects V-REP
-vi.disconnect()
+        # YouBot Trajectory
+        youbot_xd = lbr4p_xd * (1 + 0.5 * E_ * 0.015 * k_) * j_
+        youbot_ff = lbr4p_ff * (1 + 0.5 * E_ * 0.015 * k_) * j_
 
-#except Exception as exp:
-#    print(exp)
-#    print(
-#        "There was an error connecting to V-REP, please check that it is open and that the Kuka Robot is in the scene.")
-#    vi.stop_simulation()
-#    vi.disconnect_all()
+        # Modify the trajectory in order to draw a circle
+        if first_iteration:
+            first_iteration = False
+        elif np.linalg.norm(youbot_controller.get_last_error_signal()) < 0.025:
+            tc = tc + 0.01
+            rcircle = cos(tc / 2) + k_ * sin(tc / 2)
+
+        youbot_xd = youbot_xd * rcircle * tcircle
+        youbot_ff = youbot_ff * rcircle * tcircle
+
+        # Compute control signal for the arm
+        lbr4p_u = lbr4p_controller.compute_tracking_control_signal(lbr4p_q, vec8(lbr4p_xd), vec8(lbr4p_ff))
+
+        # Computer control signal for the youbot
+        youbot_base = youbot.get_chain(0)
+        youbot_base_pose = youbot_base.raw_fkm(youbot_q)
+        youbot__base_Jx = youbot_base.raw_pose_jacobian(youbot_q, 2)
+        (Jconstraint, bconstraint) = compute_constraints(youbot_base_pose, youbot__base_Jx, plane, cylinder1, cylinder2)
+        youbot_controller.set_inequality_constraint(-Jconstraint, 1*bconstraint)
+        youbot_u = youbot_controller.compute_tracking_control_signal(youbot_q, vec8(youbot_xd), vec8(youbot_ff))
+
+        # Desired joint values
+        lbr4p_q = lbr4p_q + lbr4p_u*sampling_time
+        youbot_q = youbot_q + youbot_u*sampling_time
+
+        # Send desired values
+        lbr4p_vreprobot.send_q_to_vrep(lbr4p_q)
+        youbot_vreprobot.send_q_to_vrep(youbot_q)
+
+        time.sleep(0.01)
+    print("Control finished...")
+
+    # Stops simulation in V-REP
+    print("Stopping V-REP simulation...")
+    vi.stop_simulation()
+
+    # Disconnects V-REP
+    vi.disconnect()
+
+except Exception as exp:
+    print(exp)
+    print(
+        "There was an error connecting to V-REP, please check that it is open and that the Kuka Robot is in the scene.")
+    vi.stop_simulation()
+    vi.disconnect_all()
