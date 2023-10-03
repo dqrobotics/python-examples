a7f1fa41819c3c6adf62ccb312a6ee89cca0abed
Juancho <juanjqogm@gmail.com>
2023-10-03 14:51:40 +0900

    [synch_test.py] Added a test for a valid connection and updated the try catch statement, as suggested by Murilo.

--- vrep_interface_tests/synchronous_test/synch_test.py
+++ vrep_interface_tests/synchronous_test/synch_test.py
@@ -32,7 +32,9 @@ import time
 def main() -> None:
     vi = DQ_VrepInterface()
     try:
-        vi.connect("127.0.0.1", 19997, 100, 10)
+        if not vi.connect("127.0.0.1", 19997, 100, 10):
+            raise RuntimeError("Unable to connect to CoppeliaSim.")
+            
         vi.set_synchronous(True)
         vi.start_simulation()
         time.sleep(0.1)
@@ -49,19 +51,16 @@ def main() -> None:
             vi.wait_for_simulation_step_to_end()
         print("Elapsed time: ", t)
         print("Estimated height: ", y_est, "Measured height: ", y_sim)
-        vi.stop_simulation()
-        vi.disconnect()
 
-    except KeyboardInterrupt:
+    except (Exception, KeyboardInterrupt):
         pass
 
-    except Exception as e:
-        print(e)
-
+    finally:
         vi.stop_simulation()
         vi.disconnect()
 
 
 if __name__ == "__main__":
     main()
+
 
